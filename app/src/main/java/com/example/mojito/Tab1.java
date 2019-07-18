563b976f4ee8f958ca4986d997de5eca10d78ff8
gus092 <guswl092@gmail.com>
2019-07-18 23:00:55 +0900

    return getCountryName

--- app/src/main/java/com/example/mojito/Tab1.java
+++ app/src/main/java/com/example/mojito/Tab1.java
@@ -1,22 +1,16 @@
 package com.example.mojito;
 
-import android.media.ExifInterface;
+
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
-import java.io.IOException;
-
 public class Tab1 extends Fragment {
-    public View mview;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,56 +18,18 @@ public class Tab1 extends Fragment {
 
         View view = inflater.inflate(R.layout.tab1, container, false);
 
-        mview = (TextView)getActivity().findViewById(R.id.textView);
 
-        String filename = Environment.getExternalStorageDirectory()
-                .getPath() + "/20140705_162816.jpg";
-        try {
-            ExifInterface exif = new ExifInterface(filename);
-            showExif(exif);
-        } catch (IOException e) {
-            e.printStackTrace();
-            Toast.makeText(getContext(), "Error!", Toast.LENGTH_LONG).show();
-        }
-
-        return view;
+                    Button uploadLogin = (Button)view.findViewById(R.id.uploadbtn);
+                    uploadLogin.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent i = new Intent(getActivity(), uploadPhotoActivity.class);
+                            i.putExtra("upload", "upload");
+                            startActivity(i);
+                        }
+                    });
+            return view;
     }
-
-
-    private void showExif(ExifInterface exif) {
-
-        String myAttribute = "[Exif information] \n\n";
-
-        myAttribute += getTagString(ExifInterface.TAG_DATETIME, exif);
-        myAttribute += getTagString(ExifInterface.TAG_FLASH, exif);
-        myAttribute += getTagString(ExifInterface.TAG_GPS_LATITUDE,
-                exif);
-        myAttribute += getTagString(
-                ExifInterface.TAG_GPS_LATITUDE_REF, exif);
-        myAttribute += getTagString(ExifInterface.TAG_GPS_LONGITUDE,
-                exif);
-        myAttribute += getTagString(
-                ExifInterface.TAG_GPS_LONGITUDE_REF, exif);
-        myAttribute += getTagString(ExifInterface.TAG_IMAGE_LENGTH,
-                exif);
-        myAttribute += getTagString(ExifInterface.TAG_IMAGE_WIDTH,
-                exif);
-        myAttribute += getTagString(ExifInterface.TAG_MAKE, exif);
-        myAttribute += getTagString(ExifInterface.TAG_MODEL, exif);
-        myAttribute += getTagString(ExifInterface.TAG_ORIENTATION,
-                exif);
-        myAttribute += getTagString(ExifInterface.TAG_WHITE_BALANCE,
-                exif);
-
-
-        Log.e("Image Information : ", myAttribute);
-    }
-
-    private String getTagString(String tag, ExifInterface exif) {
-        return (tag + " : " + exif.getAttribute(tag) + "\n");
-    }
-
-
 }
 
 
